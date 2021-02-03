3a6f625c9cb9c8326c0d1720207e5eb3f6e816fd
Ella77 <ksjbang@gmail.com>
2021-02-03 21:23:37 +0900

    state cycle

--- src/App.js
+++ src/App.js
@@ -4,20 +4,57 @@ import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    count: 0
   };
+  constructor(props) {
+    super(props);
+    console.log('hello');
+  }
+  componentDidMount(){
+    console.log('component rendered');
+  }
+  componentDidUpdate(){
+    console.log('I just updated')
+  }
+  componentWillUnmount(){
+    console.log('Goodbye, cruel world')
+  }
+
   add = () => {
     console.log('add');
-    this.setState(current => ({count : current.count + 1}));
+    this.setState(current => ({ count: current.count + 1 }));
   }
   minus = () => {
     console.log('minus');
-    this.setState(current => ({count : current.count - 1 }));
+    this.setState(current => ({ count: current.count - 1 }));
   }
   render() {
-    return (<div><h1>The number is : {this.state.count}</h1><button onClick={this.add}>Add</button><button onClick={this.minus}>Minus</button></div>);
+    console.log("I'm rendering");
+    return (<div>
+      <h1>The number is : {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+      </div>
+      );
   }
 }
+
+// class App extends React.Component {
+//   state = {
+//     count: 0,
+//   };
+//   add = () => {
+//     console.log('add');
+//     this.setState(current => ({count : current.count + 1}));
+//   }
+//   minus = () => {
+//     console.log('minus');
+//     this.setState(current => ({count : current.count - 1 }));
+//   }
+//   render() {
+//     return (<div><h1>The number is : {this.state.count}</h1><button onClick={this.add}>Add</button><button onClick={this.minus}>Minus</button></div>);
+//   }
+// }
 
 function Food(props) {
   return <div>
